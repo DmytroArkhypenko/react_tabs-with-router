4ecb78f0210e6c2c7972dfcbfc34ac905bbe138e
DmitryArhipenko <42931799+DmitryArhipenko@users.noreply.github.com>
2020-11-24 15:43:17 +0200

    Updates

---
+++ precache-manifest.9cd8492331cecde0ad253db20dbcf3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3591a6a6e765b261c39bbd5a7d412c9b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3d396d0e29fdf1ffa76",
+    "url": "./static/css/main.947fc389.chunk.css"
+  },
+  {
+    "revision": "244c072593500ecb8cb7",
+    "url": "./static/js/2.5e229fab.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.5e229fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d396d0e29fdf1ffa76",
+    "url": "./static/js/main.b7cf234e.chunk.js"
+  },
+  {
+    "revision": "12bee12ab4b288faf1d5",
+    "url": "./static/js/runtime-main.108729f7.js"
+  }
+]);
